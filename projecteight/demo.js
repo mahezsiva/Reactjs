f0b9c83c3b951cb14df4bac133231ef4a5ca67f1
mahezsiva <86237516+mahezsiva@users.noreply.github.com>
2021-06-22 09:01:35 +0530

    Add files via upload

---
+++ projecteight/demo.js
@@ -0,0 +1,16 @@
+
+let x = "787";
+let y = "78";
+
+if (x == y) {
+    console.log("Values are same ");//== compares only the values
+}
+else {
+    console.log("fvalues are different");
+}
+if (x === y) {
+    console.log("data types are same");
+}
+else {
+    console.log("data type is different");//compares values and datatypes
+}
